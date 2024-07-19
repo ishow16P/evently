2f1c511b2f6b17c7660211f881c9f3a9a99bdf12
Sutee P <posteiei10@gmail.com>
2024-07-19 10:55:20 +0700

    Fix bug

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -59,8 +59,8 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
         {categories.length > 0 &&
           categories.map((category) => (
             <SelectItem
-              key={category._id}
-              value={category._id}
+              key={category._id as string}
+              value={category._id as string}
               className="select-item p-regular-14"
             >
               {category.name}
